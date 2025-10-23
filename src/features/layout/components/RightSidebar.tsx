2742314a969e21b55284ce28320907f07cb6f022
Ammar Yasser <143096817+AMYasserF@users.noreply.github.com>
2025-10-23 12:39:45 +0300

    feat(layout): complete sidebar responsive and layout unit testing

--- src/features/layout/components/RightSidebar.tsx
+++ src/features/layout/components/RightSidebar.tsx
@@ -3,14 +3,12 @@ import React from 'react';
 import WhatIsHappening from './WhatIsHappening';
 import WhoToFollow from './WhoToFollow';
 import Footer from './Footer';
-import { SearchInput } from '@/components/ui/input';
+import SearchBar from './SearchBar';
 
 export default function RightSidebar() {
   return (
-    <aside className="hidden lg:flex sticky right-0 top-0 w-sm h-screen flex-col gap-4 px-4 pt-1  bg-black overflow-y-auto">
-      <div className="px-2 mb-2 mt-2">
-        <SearchInput placeholder="Search" />
-      </div>
+    <aside className="hidden lg:flex sticky right-0 top-0 w-full h-screen flex-col gap-4 px-4 pt-1 bg-black overflow-y-auto">
+      <SearchBar />
       <WhatIsHappening />
       <WhoToFollow />
       <Footer />
