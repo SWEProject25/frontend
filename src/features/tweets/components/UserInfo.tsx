65f8a4d829225fa6da2fc62c85484cbd16d8f303
ahmedfathy0-0 <ahmedfathi20044002@gmail.com>
2025-11-18 17:49:19 +0200

    fix: update ProfileCard prop to use userId instead of data

--- src/features/tweets/components/UserInfo.tsx
+++ src/features/tweets/components/UserInfo.tsx
@@ -81,7 +81,7 @@ export default function UserInfo({
               e.stopPropagation();
             }}
           >
-            {data.name && data.username && <ProfileCard data={data} />}
+            {data.name && data.username && <ProfileCard userId={data.id} />}
           </div>
         )}
       </div>
@@ -118,7 +118,7 @@ export default function UserInfo({
               e.stopPropagation();
             }}
           >
-            {data.name && data.username && <ProfileCard data={data} />}
+            {data.name && data.username && <ProfileCard userId={data.id} />}
           </div>
         )}
       </div>
