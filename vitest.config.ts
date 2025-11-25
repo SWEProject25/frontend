59cbd06a83dc70e7a7052a11c1e1d24621126075
KarimZakzouk <karimzakzouk69@gmail.com>
2025-11-25 03:00:09 +0200

    feat: add SonarQube configuration and mock localStorage for testing

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,11 +6,24 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment: 'jsdom',
-    setupFiles: ['./src/test/setup.ts'],
+    setupFiles: ['./src/test/globals.ts', './src/test/setup.ts'],
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     exclude: ['node_modules', 'dist', '.idea', '.git', '.cache'],
     globals: true,
     css: true,
+    coverage: {
+      provider: 'v8', // or 'istanbul' if you prefer
+      reporter: ['text', 'json', 'html', 'lcov'], // Add lcov for SonarQube
+      reportsDirectory: './coverage',
+      exclude: [
+        'node_modules/',
+        'src/test/',
+        '**/*.spec.ts',
+        '**/*.test.ts',
+        '**/*.spec.tsx',
+        '**/*.test.tsx',
+      ],
+    },
   },
   resolve: {
     alias: {
